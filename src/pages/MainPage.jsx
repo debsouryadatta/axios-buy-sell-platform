df9bf9a0ad38acb3c357d17a40970982d204cc7a
debsouryadatta <debsouryadatta@gmail.com>
2023-12-14 15:48:25 +0530

    Firebase Auth Access modified

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -13,16 +13,16 @@ const MainPage = () => {
                 <Nav />
             </div>
 
-            <Parallax pages={2} style={{ top: '0', left: '0' }} className='bg-blue-gray-800'>
+            <Parallax pages={2.5} style={{ top: '0', left: '0' }} className='bg-blue-gray-800'>
 
                 <ParallaxLayer offset={0} speed={1.3} factor={1} style={{
                     backgroundImage: `url('https://res.cloudinary.com/diyxwdtjd/image/upload/v1702463792/Project%20use/Untitled_design_cylxgw.png')`,
                     backgroundSize: 'cover',
                 }} />
-                <ParallaxLayer offset={0.98} speed={1.5} style={{
+                <ParallaxLayer offset={0.98} speed={1.5} factor={1} style={{
                     backgroundImage: `url('https://res.cloudinary.com/diyxwdtjd/image/upload/v1701082646/Project%20use/Untitled_design_2_kimeki.png')`,
                     backgroundSize: 'cover',
-                    height: '140vh',
+                    height: '100%',
                     zIndex: '10',
                 }}>
                     <motion.div
@@ -30,6 +30,7 @@ const MainPage = () => {
                         transition={{ duration: 0.5 }}>
                         <TabsComponent />
                     </motion.div>
+
                 </ParallaxLayer>
 
 
@@ -39,7 +40,10 @@ const MainPage = () => {
                 </ParallaxLayer>
 
 
-                <ParallaxLayer offset={1} speed={1.5} factor={0.2} className='z-10' >
+                <ParallaxLayer offset={1.5} speed={1.5} factor={0.2} style={{
+                    height: '100%',
+                    zIndex: '10',
+                }} >
                     <Footer />
                 </ParallaxLayer>
             </Parallax>
